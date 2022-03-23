b59f196c9b6ffd66cf5fc4fe6535d6e39112e1ee
Cristian Parada <cmpmendoza12@gmail.com>
2022-03-23 16:57:07 -0600

    Realizacion de escenarios para resta, multiplicacion y division

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.When;
 public class CalculadoraTest {
 
     //Iniciamos una variable tipo entero y la inicializamos en cero
-    int suma = 0;
+    int resultado = 0;
 
     @Given("que tengo una calculadora")
     public void que_tengo_una_calculadora() {
@@ -17,17 +17,37 @@ public class CalculadoraTest {
         System.out.println("Realizando suma...");
 
         //Creando la suma de dos variables
-        suma = int1 + int2;
+        resultado = int1 + int2;
 
     }
+    @When("resto {int} y {int}")
+    public void resto_y(Integer int1, Integer int2) {
+        System.out.println("Restando...");
+        //Creando dos variables para la resta
+        resultado = int1 - int2;
+    }
+    @When("multiplico {int} y {int}")
+    public void multiplico_y(Integer int1, Integer int2) {
+        //Creando dos variables para la multiplicacion
+        resultado = int1 * int2;
+    }
+    @When("divido {int} y {int}")
+    public void divido_y(Integer int1, Integer int2) {
+        //Creando dos variables para la division
+        resultado = int1 * int2;
+    }
+
     @Then("el resultado es {int}")
     public void el_resultado_es(Integer int1) {
         System.out.println("Mostrando resultado...");
-        if (suma == int1){
-            System.out.println("La suma es :" + suma);
+        if (resultado == int1){
+            System.out.println("Enhorabuena! El resultado es el esperado :" + resultado);
         }else{
             System.out.println("Error se esperaba el resultado.." + int1);
         }
+        //Validar que sea igual el resultado con el int1 esto es lo que en realidad valida el testing
 
+
+        //Assert.assertEquals(resultado, int1);
     }
 }
--- src/test/java/cal.feature
+++ src/test/java/cal.feature
@@ -4,3 +4,21 @@ Feature: Test de una calculadora
     Given que tengo una calculadora
     When sumo 2 y 4
     Then el resultado es 6
+
+
+  Scenario: realizar una resta
+    Given que tengo una calculadora
+    When resto 5 y 2
+    Then el resultado es 3
+
+
+  Scenario: realizar una multiplicacion
+    Given que tengo una calculadora
+    When multiplico 3 y 5
+    Then el resultado es 15
+
+
+  Scenario: realizar una division
+    Given que tengo una calculadora
+    When divido 20 y 4
+    Then el resultado es 2
